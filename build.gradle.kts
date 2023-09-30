2770c926fa6e102c1a5db920079bb3e2e9546c45
Felipe <felipemonteirose@gmail.com>
2023-09-30 18:01:27 -0300

    Ajusta build gradle do projeto

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,10 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath(libs.google.services)
-        classpath(libs.firebase.crashlytics.gradle)
-        classpath(libs.kotlin.gradle)
-        classpath(libs.hilt.plugin)
-    }
-}// Top-level build file where you can add configuration options common to all sub-projects/modules.
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.firebase.crashlytics.gradle) apply false
+    alias(libs.plugins.google.services) apply false
+    alias(libs.plugins.hilt) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.ksp) apply false
 }
