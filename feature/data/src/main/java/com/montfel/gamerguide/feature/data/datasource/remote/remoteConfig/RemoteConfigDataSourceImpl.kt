c25374936372d977fbe880104fa73734f9a8bcd5
Felipe <felipemonteirose@gmail.com>
2024-04-03 19:22:24 -0300

    Refatora módulos de core

--- feature/data/src/main/java/com/montfel/gamerguide/feature/data/datasource/remote/remoteConfig/RemoteConfigDataSourceImpl.kt
+++ feature/data/src/main/java/com/montfel/gamerguide/feature/data/datasource/remote/remoteConfig/RemoteConfigDataSourceImpl.kt
@@ -2,12 +2,23 @@ package com.montfel.gamerguide.feature.data.datasource.remote.remoteConfig
 
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import javax.inject.Inject
-import kotlinx.coroutines.tasks.await
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 internal class RemoteConfigDataSourceImpl @Inject constructor(
     private val firebaseRemoteConfig: FirebaseRemoteConfig,
 ) : RemoteConfigDataSource {
-    override suspend fun init(): Boolean = firebaseRemoteConfig.fetchAndActivate().await()
+    override suspend fun init(): Boolean {
+        return suspendCoroutine { continuation ->
+            firebaseRemoteConfig.fetchAndActivate().addOnCompleteListener {
+                if (it.isComplete && it.isSuccessful) {
+                    continuation.resume(it.result)
+                } else {
+                    continuation.resume(false)
+                }
+            }
+        }
+    }
 
     override suspend fun getString(key: String): String {
         return firebaseRemoteConfig.getString(key)
