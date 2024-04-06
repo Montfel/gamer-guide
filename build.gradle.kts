32aa9dfe835e5b965ef998d361203f7207b9c3cc
Felipe <felipemonteirose@gmail.com>
2024-04-06 19:56:32 -0300

    Atualiza dependencias

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,5 +6,6 @@ plugins {
     alias(libs.plugins.google.services) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.ksp) apply false
 }
