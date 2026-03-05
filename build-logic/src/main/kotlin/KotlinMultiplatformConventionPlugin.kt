b565e611c7cec3836425a0bd322322fab05f3f5b
Felipe <felipemonteirose@gmail.com>
2026-03-04 21:13:38 -0300

    migra para gradle 9

--- build-logic/src/main/kotlin/KotlinMultiplatformConventionPlugin.kt
+++ build-logic/src/main/kotlin/KotlinMultiplatformConventionPlugin.kt
@@ -1,10 +1,7 @@
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
-import org.jetbrains.kotlin.gradle.dsl.kotlinExtension
-import kotlin.text.get
 
 class KotlinMultiplatformConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) = with(target) {
